5f8ee2e6feebe5976045bea1fff58a963e591fe1
Nick Jiang <njiang1209@ufl.edu>
2015-12-29 01:47:22 -0500

    sidebar

---
+++ Site/assets/js/custom2.js
@@ -0,0 +1,22 @@
+(function($){
+
+  $(document).ready(function() {
+    $(".sidebar").offcanvas('show');
+  });
+
+  var alreadyHidden = false;
+  $( window ).scroll(function() {
+    if (!alreadyHidden && $(window).scrollTop() > 0) {
+      $(".sidebar").offcanvas('hide');
+      alreadyHidden = true;
+    }
+
+    if ($(window).scrollTop() == 0) {
+      $(".sidebar").offcanvas('show');
+      alreadyHidden = false;
+    }
+  });
+
+
+
+})(jQuery);
