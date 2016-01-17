79e887b739e8959842e1a3be8235e88f8d3ee7ba
Nick Jiang <njiang1209@ufl.edu>
2016-01-17 16:58:13 -0500

    new stuff

--- Site/assets/js/custom2.js
+++ Site/assets/js/custom2.js
@@ -1,20 +1,20 @@
 (function($){
 
-  $(document).ready(function() {
-    $(".sidebar").offcanvas('show');
-  });
+  // $(document).ready(function() {
+  //   $(".sidebar").offcanvas('show');
+  // });
 
   var alreadyHidden = false;
   $( window ).scroll(function() {
-    if (!alreadyHidden && $(window).scrollTop() > 0) {
-      $(".sidebar").offcanvas('hide');
-      alreadyHidden = true;
-    }
+    // if (!alreadyHidden && $(window).scrollTop() > 0) {
+    //   $(".sidebar").offcanvas('hide');
+    //   alreadyHidden = true;
+    // }
 
-    if ($(window).scrollTop() == 0) {
-      $(".sidebar").offcanvas('show');
-      alreadyHidden = false;
-    }
+    // if ($(window).scrollTop() == 0) {
+    //   $(".sidebar").offcanvas('show');
+    //   alreadyHidden = false;
+    // }
   });
 
 
