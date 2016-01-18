78dd3e928352f6a36d668a01efe6d7474df8e5e9
Nick Jiang <njiang1209@ufl.edu>
2016-01-18 01:22:12 -0500

    filtering

--- Site/assets/js/data.js
+++ Site/assets/js/data.js
@@ -1,70 +1,70 @@
 var projects = [
   { 
     title: "Hilo Hawaiian Products", 
-    subtitle: "branding, packaging, identity",
+    tags: ["branding", "packaging", "identity"],
     thumbnail: "images/hiloall.png",
     images: ["images/hilobox.png", "images/hilobox2.png", "images/hilobox3.png", "images/hilocandle.png", "images/hilocandle3.png", "images/hilooil.png"],
     description: "Hilo is a fictional family owned company based in Hilo Island Hawaii that make products using fresh resources grown from the rich soils of the Mauna Loa Volcano. Their products include an ohelo berry herbal tea, macadamia nut cooking oil, and kukui nut candle. The pheonix represented in their packaging illustrates their beliefs in natural cycle of destruction and creation. Hawaiian beliefs and philosophies are srongly tied with their observations of nature. In destruction there is always hope for new life in the same way that their products are born from the ashes of a volcano."
   },
   { 
     title: "Haulbox Startup", 
-    subtitle: "branding, mobile app, web",
+    tags: ["branding", "mobile app", "web"],
     thumbnail: "images/haulboxthum.png",
     images: [],
     description: "Stuff"
   },
   { 
     title: "Midnight Cookies Menu", 
-    subtitle: "identity, menu design",
+    tags: ["identity", "menu design"],
     thumbnail: "images/midnightall.jpg",
     images: [],
     description: "Stuff"
   },
   { 
     title: "Epic", 
-    subtitle: "E-Book, illustration",
+    tags: ["E-Book", "illustration"],
     thumbnail: "images/epicthumb.png",
     images: [],
     description: "Stuff"
   },
   { 
     title: "Haulbox Startup", 
-    subtitle: "branding, mobile app, web",
+    tags: [],
     thumbnail: "images/haulboxthum.png",
     images: [],
     description: "Stuff"
   },
   { 
     title: "Midnight Cookies Menu", 
-    subtitle: "identity, menu design",
+    tags: [],
     thumbnail: "images/midnightall.jpg",
     images: [],
     description: "Stuff"
   },
   { 
     title: "Epic", 
-    subtitle: "E-Book, illustration",
+    tags: [],
     thumbnail: "images/epicthumb.png",
     images: [],
     description: "Stuff"
   },
   { 
     title: "Haulbox Startup", 
-    subtitle: "branding, mobile app, web",
+    tags: [],
     thumbnail: "images/haulboxthum.png",
     images: [],
     description: "Stuff"
   },
   { 
     title: "Midnight Cookies Menu", 
-    subtitle: "identity, menu design",
+    tags: [],
     thumbnail: "images/midnightall.jpg",
     images: [],
     description: "Stuff"
   },
   { 
     title: "Epic", 
-    subtitle: "E-Book, illustration",
+    tags: [],
     thumbnail: "images/epicthumb.png",
     images: [],
     description: "Stuff"
@@ -73,14 +73,19 @@ var projects = [
 
 
 var portfolio_link = function(project, index) {
-  return  '<article class="work-item travel video">' +
+
+  var tag_classes = project.tags;
+  for (var i = 0; i < tag_classes.length; i++) {
+    tag_classes[i] = tag_classes[i].replace(" ", "-");
+  }
+  return  '<article class="work-item ' + tag_classes.join(" ") + '">' +
             '<div class="work-wrapper">' +
               '<div class="work-thumbnail">' +
                 '<img src="' + project.thumbnail + '" alt="">' +
                   '</div>' +
                   '<div class="work-caption">' +
                     '<h3 class="work-title font-alt">' + project.title + '</h3>' +
-                    '<span class="work-category font-serif"><a href="#">' + project.subtitle + '</a></span>' +
+                    '<span class="work-category font-serif"><a href="#">' + project.tags.join(", ") + '</a></span>' +
                   '</div>' +
                   '<a href="#" class="work-link" id="' + index + '"></a>' +
                 '</div>' +
