913efbcff4ddd558961673f0a6a4393b86b23483
kimestoesta <kimestoesta@gmail.com>
2016-01-29 08:33:13 -0500

    friday

--- Site/assets/js/data.js
+++ Site/assets/js/data.js
@@ -8,17 +8,17 @@ var projects = [
   },
   { 
     title: "EyeSpy.", 
-    tags: ["branding", "mobile app", "web"],
+    tags: ["identity", "mobile app", "web"],
     thumbnail: "images/EyeSpyLogin.gif",
-    images: ["images/EyeSpyRiddler.jpg","images/EyeSpy-elephant.jpg","images/EyeSpy-Menu.jpg", "images/EyeSpy-Vessel.jpg","images/EyeSpy_Welcome.jpg","images/EyeSpy-done.jpg","images/EyeSpy-concept.jpg","images/EyeSpy-userprofile.jpg","images/EyeSpy-flatpurple.jpg","images/EyeSpy-demoview.jpg"],
+    images: ["images/EyeSpyRiddler.jpg","images/EyeSpy-concept.jpg","images/EyeSpy-userprofile.jpg","images/EyeSpy-elephant.jpg","images/EyeSpy-Menu.jpg","images/EyeSpy-flatpurple.jpg", "images/EyeSpy-Vessel.jpg","images/EyeSpy_Welcome.jpg","images/EyeSpy-done.jpg","images/EyeSpy-demoview.jpg"],
     description: "EyeSpy. is a project I did with my classmate, Paige Carl, for the Harn Museum of Art. The project challenge was to design an app for the Harn Museum that focused on the Asian collection emphasizing both mobility and sensor technology. Our solution led to the creation of an app, EyeSpy, which we prototyped with a focus on the Asian collection of the Harn but could also be expanded for future collections. <br><br> EyeSpy is designed to be the Harn Museum’s interactive mystery adventure game app. Users have only 60 minutes to physically explore the museum exhibit to search for clues and figure out riddles to solve the mystery. During the project I had a large focus on the story, concepting, User Interface design, and illustration. Paige and I worked together to create the information architecture and user experience of the game and split screen production design. <br><br> Try the prototype on invision: http://invis.io/UX2RU5KV6</a>"
   },
-  { 
-    title: "Midnight Cookies Menu", 
-    tags: ["identity", "menu design"],
-    thumbnail: "images/cards-characters.jpg",
-    images: [],
-    description: "Stuff"
+{ 
+    title: "Goode Chocolate Bars", 
+    tags: ["packaging", "branding"],
+    thumbnail: "images/goode-above.jpg",
+    images: ["images/goodechocolate.jpg","images/goode-above.jpg"],
+    description: "The Goode Chocolate Company was a fictional chocolate company for a student project celebrating 100 years of bussiness. To honor their anniversary this packaging design pays tribute to some of the company’s best years, the roaring 20s. The company sells three flavors dark, milk, and white chocolate each distinguished by a different colored foil revealed through the New York City skyline where the company originated."
   },
   { 
     title: "Relient K Band Tour", 
@@ -29,18 +29,18 @@ var projects = [
   },
   { 
     title: "Haulbox Startup", 
-    tags: [],
+    tags: ["branding", "mobile app", "web"],
     thumbnail: "images/haulboxthum.png",
-    images: [],
-    description: "Stuff"
+    images:["images/haulboxweb.png", "images/haulboxdemo.png","images/haulboxicons.png","images/haulboxapp.png"],
+    description: "Haulbox is a StartUp Company that originated in Gainesville, FL Starting from scratch as their Graphic Designer I created their entire visual branding from their logo to their promotional items to their web and mobile app graphics."
   },
-   { 
-    title: "Cards", 
-    tags: [],
-    thumbnail: "",
-    images: [],
-    description: "Stuff"
-  },
+  { 
+    title: "The Faces of Luck", 
+    tags: ["identity", "menu design"],
+    thumbnail: "images/cards-characters.jpg",
+    images: ["images/cardsflat.png",],
+    description: "Faces of Luck was created for a student project. The illustration depicts an exploded view of playing cards but also holds meaning in the depictions of the King and Queen. Cards can associated with gambling and the images work to subtly show the cycles of gambling, you win some and you lose some. Gambling is a non-discriminatory activity and can effect both male and female. The illustration portrays this serious topic in a lighthearted style and allows people to entertain the thought of lows and highs in gambling. This project will later be expanded into a card deck."
+  }, 
   
 ];
 
