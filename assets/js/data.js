9eea2b5c8b754b3e480ddda6bfa77db5d3793823
kimestoesta <kimestoesta@gmail.com>
2016-02-21 13:42:12 -0500

    descriptions

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,50 +1,55 @@
 var projects = [
   {
     title: "That One Spot", 
-    tags: ["branding", "packaging"],
+    tags: ["branding", "print", "packaging"],
     thumbnail: "images/spot-takeout.jpg",
     images: ["images/spot-all.jpg", "images/tos_mat.jpg", "images/spot-bigmenu.jpg", "images/spot-businesscards.jpg", "images/spot-takeout1.jpg","images/spot-takeout2.jpg", "images/spot-items.jpg", "images/spot-all2.jpg",],
-    description: "That One Spot is a family owned burger restaurant located in Ocoee, FL. They are known for their well crafted and customizable burgers with a variety of meat options but also provide a variety of salads and sides. While their ingredients are fresh and local they aim to be a fast casual spot for everyone to grab a bite. Keeping their brand in mind I was hired to rebrand the restaurant to more closely reflect their voice today and as they look to grow."
+    description: "That One Spot is a rebrand I did for a client running a family owned burger restaurant located in Ocoee, FL. They are known for their well crafted and customizable burgers with a variety of meat options but also provide a variety of salads and sides. While their ingredients are fresh and local they aim to be a fast casual spot for everyone to grab a bite. Keeping their brand in mind I was hired to rebrand the restaurant to more closely reflect their voice today and as they look to grow."
   },
   { 
     title: "EyeSpy.", 
-    tags: ["interactive", "app"],
+    tags: ["digital", "app", "interactive"],
     thumbnail: "images/EyeSpyLogin.gif",
     images: ["images/EyeSpyRiddler.jpg","images/EyeSpy-Concept.jpg","images/EyeSpy-UserProfile.jpg","images/EyeSpy-elephant.jpg","images/EyeSpy-menu.jpg","images/EyeSpy-FlatPurple.jpg", "images/EyeSpy-Vessel.jpg","images/EyeSpy_Welcome.jpg","images/EyeSpy-done.jpg","images/EyeSpy-DemoView.jpg"],
-    description: "EyeSpy is a project I did with my classmate, Paige Carl, for the Harn Museum of Art. The project challenge was to design an app for the Harn Museum that focused on the Asian collection emphasizing both mobility and sensor technology. Our solution led to the creation of an app, EyeSpy, which we prototyped with a focus on the Asian collection of the Harn but could also be expanded for future collections. <br><br> EyeSpy is designed to be the Harn Museum’s interactive mystery adventure game app. Users have only 60 minutes to physically explore the museum exhibit to search for clues and figure out riddles to solve the mystery. During the project I had a large focus on the story, concepting, User Interface design, and illustration. Paige and I worked together to create the information architecture and user experience of the game and split screen production design. <br><br> Try the prototype on <a href='http://invis.io/UX2RU5KV6'>invision</a>"
+    description: "EyeSpy is a project I did with my classmate, Paige Carl, for the Harn Museum of Art in Gainesville, FL. The project challenge was to design an app for the Harn Museum that focused on the Asian collection emphasizing both mobility and sensor technology. Our solution led to the creation of an app, EyeSpy, which we prototyped with a focus on the Asian collection of the Harn but could also be expanded for future collections. <br><br> EyeSpy is designed to be the Harn Museum’s interactive mystery adventure game app. Users have only 60 minutes to physically explore the museum exhibit to search for clues and figure out riddles to solve the mystery. During the project I had a large focus on the story, concepting, research, User Interface design, and illustration. Paige and I worked together to create the information architecture and user experience of the game and split screen production design. <br><br> Try the prototype on <a href='http://invis.io/UX2RU5KV6'>invision</a>"
   },
 { 
     title: "RecSports Small Group Training", 
-    tags: ["print", "digital"],
+    tags: ["print", "digital", "campaign"],
     thumbnail: "images/findfit-thumbnail.jpg",
+<<<<<<< Updated upstream
     clips: ['<iframe src="https://player.vimeo.com/video/155912657" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'],
     images: ["images/findfit-multiple.jpg","images/findfit-double.jpg", "images/findfit-booklet.gif", "images/findfit-social.jpg", "images/findfit-table.jpg", "images/findfit-sides.jpg", "$", "images/findfit-tvs.jpg",],
     description: 'The marketing team for Recreation Sports at the University of Florida is comprised of designers, videographers, photographers, and media specialists. Working in-house we service many different departments within RecSports like clients making sure to boost visibility and spread awareness of several campaigns or events.'
+=======
+    images: ["images/findfit-multiple.jpg","images/findfit-double.jpg", "images/findfit-booklet.gif", "images/findfit-social.jpg", "images/findfit-table.jpg", "images/findfit-sides.jpg", "images/findfit-tvs.jpg",],
+    description: "The marketing team for Recreational Sports at the University of Florida is comprised of designers, videographers, photographers, and media specialists. I work in-house where we service many different departments within RecSports like clients making sure to boost visibility and spread awareness of several campaigns or events. This was a 4 page booklet I designed for Small Group Training. It includes the different formats offered for that semester as well as the rates and calendar of all the classes being offered. For the campagn we printed and distributed the 4 pager at all our facilities and ran the campaign through our website and social media channels. I also animated the campaign for our digital outlets that play throughout the day on severals screens throughout the fitness facilites."
+>>>>>>> Stashed changes
   },
   { 
     title: "Relient K Band Tour", 
     tags: ["branding", "illustration"],
     thumbnail: "images/relientk-digitalad.gif",
-    images: ["images/relientk-all2.jpg", "images/relientk-posters.jpg", "images/relientk-trio.jpg", "images/relientk-CD.jpg", "images/relientk-book.jpg", "images/relientk-bookdetails.jpg", "images/relientk-pass.jpg", "images/relientk-lyricbook.jpg", "images/relientk-all.jpg",],
-    description: "Relient K - Only Fools Know Everything was a retrospective tour campaign for a student project. For this retrospective tour I wanted to capture the essence of growing up and making mistakes but having fun doing it. The idea is that because you were a fool you messed up and it has made you who you are today. <br><br> This project was selected to be displayed at Ligature25."
+    images: ["images/relientk-all.jpg", "images/relientk-posters.jpg", "images/relientk-trio.jpg", "images/relientk-CD.jpg", "images/relientk-book.jpg", "images/relientk-bookdetails.jpg", "images/relientk-pass.jpg", "images/relientk-lyricbook.jpg", "images/relientk-all2.jpg",],
+    description: "Relient K - Only Fools Know Everything was a retrospective tour campaign I created for a student project. For this retrospective tour I wanted to capture the essence of growing up and making mistakes but having fun doing it which is how the band has characterized themselves through their music over the years. The idea is that because you were a fool you messed up and it has made you who you are today. <br><br>The tour is accomanied by a book release in which the band writes on their journey and process of writing their songs as well as a special edition CD release that includes a lyric booklet of all the songs on the album. Fans and new listeners will notice that each song is accompanied by illstrations relating to lyrics within the songs. These illustrations comprise the identity of the tour itself shown through posters, VIP passes, book and album art. "
   },
   { 
     title: "Haulbox Startup", 
     tags: ["branding", "mobile app", "web"],
     thumbnail: "images/haulboxthum.png",
-    images:["images/haulboxweb.png", "images/haulboxdemo.png","images/haulboxicons.png","images/haulboxapp.png"],
-    description: "Haulbox is a StartUp Company that originated in Gainesville, FL. As their graphic designer I collaborated with the founders to ideate and create their entire visual branding system. From designing the logo to creating the icons for the user interface of the app and site I wanted to reflect the character of the company and its founders."
+    images:["images/haulboxicons.png", "images/haulboxweb.png", "images/haulboxdemo.png","images/haulboxapp.png"],
+    description: "Haulbox is a StartUp Company that originated in Gainesville, FL. As their lead graphic designer I collaborated with the founders to ideate and create their entire visual branding system from the ground up. From designing the logo to creating the icons for the user interface of the app and site I wanted to reflect the company's convenient, friendly, and easy service. During this experience I worked with entrepenuers and programmers directly in an open studio layout at the startup incubator Starterspace. I was later promoted to work with other startups within Starterspace as a creative director and designer."
   },
   { 
     title: "The Kailune Chronicles", 
-    tags: ["branding", "mobile app", "web"],
+    tags: ["concept", "storytelling", "illustration"],
     thumbnail: "images/imaginations-logo.jpg",
     images:["images/imaginations-1.jpg", "images/imaginations-2.jpg","images/imaginations-3.jpg","images/imaginations-4.jpg", "images/imaginations-5.jpg","images/imaginations-6.jpg",],
-    description: "."
+    description: "The Kailune Chronicles is a project designed for the Disney Imaginations Competition. Wanting to enter the competition I recruited 3 other students - Elizabeth Rhodes, Brandon Kocher, and Shari Brown. Together as a team we imagined and concepted The Kailune Chronicles as the solution to the Imaginations Competition project challenge. The 2015 challenge was to create a form of traveling entertainment that could be set up and broken down within a day. Not everyone has the means to visit Disney World ot Disneyland so this traveling entertanment was meant to make a tour of the United States stopping in several cities for 2-3 days.<br><br>This was the final presentation we submitted. Together we collaborated and came up with the story during a series of meetings. In production we split up our duties to create the final presentation. My contributions to the project were creating the logo, the official park map, helping with concept and UX of the app, concept art of the domes and airships, research of airship technology, Maya 3-D model of the airships, and the storyboard art sequence of the final show. Over all I worked as the team leader and art director making calendars and controlling task management of the group."
   },
   { 
     title: "The Faces of Luck", 
-    tags: ["identity", "menu design"],
+    tags: ["digital", "print", "illustration"],
     thumbnail: "images/Cards-characters.jpg",
     images: ["images/cardsflat.png",],
     description: "Faces of Luck was created for a student project. The illustration depicts an exploded view of playing cards but also holds meaning in the depictions of the King and Queen. Cards can associated with gambling and the images work to subtly show the cycles of gambling, you win some and you lose some. Gambling is a non-discriminatory activity and can effect both male and female. The illustration portrays this serious topic in a lighthearted style and allows people to entertain the thought of lows and highs in gambling. This project will later be expanded into a card deck."
@@ -54,7 +59,7 @@ var projects = [
     tags: ["identity", "menu design"],
     thumbnail: "images/jelly-bag.jpg",
     images: ["images/jelly-all.jpg", "images/jelly-cards.jpg", "images/jelly-poster.gif", "images/jelly-flavordetails.jpg", "images/jelly-flavorbag.jpg"],
-    description: "."
+    description: "This student project is "
   }, 
   { 
     title: "Goode Chocolate Bars", 
