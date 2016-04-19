34a54733fc450814cee9e371321845d3b0ac1e98
kimestoesta <kimestoesta@gmail.com>
2016-04-19 17:50:01 -0400

    boop

--- assets/js/data.js
+++ assets/js/data.js
@@ -54,7 +54,7 @@ var projects = [
     title: "Jelly Belly", 
     tags: ["branding", "illustration", "print"],
     thumbnail: "images/jelly-bag.jpg",
-    images: ["images/jelly-all.jpg", "images/jelly-cards.jpg", "images/jelly-poster.gif", "images/jelly-flavordetails.jpg", "images/jelly-flavorbag.jpg"],
+    images: ["images/jelly-all.jpg", "images/jelly-cards.jpg", "images/jelly-poster.gif", "images/jelly-flavordetails.jpg", "images/jelly-flavorbag.jpg", "images/jelly-bags.jpg", "images/jelly-bagdance.gif"],
     description: "This student project is the beginning of a rebrand for Jelly Belly, the makers of gourmet jelly beans. After researching Jelly Belly's history I learned that jelly beans were actually made popular by President Ronald Reagan during his time in office. Since then Jelly Belly has kept their brand identity the same so I wanted to bring new life to the brand and make it more relevant to today. As the generation of Ronald Reagan ages I wanted to focus to a new and younger audience. Here I have have created a new logo, mascot, flavor guide, and packaging concept. The project will later extend to show character animations and a larger packaging system."
   }, 
   { 
