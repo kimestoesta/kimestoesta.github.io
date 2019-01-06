f5685b403b52d4dbef97800d971087c8e69ff9f8
kimestoesta <kimestoesta@github.com>
2019-01-05 22:15:08 -0800

    Added Projects
    gitmerge btf

--- kim_portfolio/src/components/main/Data.js
+++ kim_portfolio/src/components/main/Data.js
@@ -6,61 +6,234 @@ class Data {
 
   static projects = [
     {
-      title: 'That One Spot',
-      tags: ['branding', 'print', 'packaging'],
-      thumbnail: 'images/spot-takeout.jpg',
+      title: 'Git Merge',
+      tags: ['branding'],
+      thumbnail: 'images/gitmerge-badge.jpg',
       rows: [
         [
           new TextNode(
-            'That One Spot is a rebrand I did for a client running a family owned burger restaurant located in Ocoee, FL. They are known for their well crafted and customizable burgers with a variety of meat options but also provide a variety of salads and sides. While their ingredients are fresh and local they aim to be a fast casual spot for everyone to grab a bite. Keeping their brand in mind I was hired to rebrand the restaurant to more closely reflect their voice today and as they look to grow.',
+            'Git Merge is a full-day conference offering technical content and user case studies, plus a day of workshops for Git users of all levels. I had the opportunity to lead the design for Git Merge conferences 2017 and 2018. It was an interesting challenge to reinvision the design in 2018 since doing it in 2017. The problem: How do I make this brand look the same but also different?',
             'TLDR'),
           new ImageNode(
-            'images/spot-all.jpg',
+            'images/gitmerge-pattern.jpg',
             2,
           )
         ],
         [
-          new ImageNode('images/tos_mat.jpg'),
-          new ImageNode('images/spot-bigmenu.jpg'),
+          new TextNode(
+            ' ','Git Merge 2017 Brand'),
         ],
         [
-          new ImageNode('images/spot-businesscards.jpg'),
-          new TextNode(),
-          new ImageNode('images/spot-takeout1.jpg',),
+          new ImageNode('images/gitmerge-2017brand.jpg'),
         ],
         [
-          new ImageNode('images/spot-takeout2.jpg'),
-          new ImageNode('images/spot-items.jpg'),
-          new ImageNode('images/spot-all2.jpg'),
+          new TextNode(
+              '- Engage with the Git Community \n - Create a rewarding attendee experience \n - Create a Platform for Speakers and exhibitors','Goals Git Merge 2018'),
+          new TextNode(
+            '- Branding \n - Marketing on social media \n - Swag items \n - Signage and way finding exhibitors','Responsibilities'),
+          new TextNode(
+            'How do we evolve the Git Merge brand from last year? \n 2017 branding took a micro look into git branches, git lines were zoomed in. \n Consider: \n - zooming out \n - what the git lines mean in a larger context?','Explorations'),
+        ],
+        [
+          new ImageNode('images/gitmerge-explorations2018.jpg'),
+        ],
+        [
+          new TextNode(
+            'This year we zoom out and thought about what these git lines mean. The lines are part of building a project so I settled on squares on a grid that could be visually built and rearranged. Grouping squares into tetris like shapes also helped visualize the idea of building and how projects are all different but can be created through the same process of git.','Brand Guide'),
+            new TextNode(
+            ' '),
+        ],
+        [
+          new ImageNode('images/gitmerge-brand2018.jpg'),
+        ],
+        [
+          new ImageNode('images/gitmerge-site2018.jpg'),
+        ],
+        [
+          new TextNode(
+            'Git Merge 2018 happened in Barcelona at Convent Dels Àngels at the MACBA. The historical building that was once a church and is now preserved and part of their museum of contemporary art. I flew to Barcelona to meet with vendors, plan directional signage, lighting, and discuss any restrictions the venue had on branding the space. ','Site Visit'),
+          new TextNode(
+            '  '),
+        ],
+        [
+          new ImageNode(
+            'images/gitmerge-venue.jpg')
+        ],
+        [
+          new TextNode(
+            '  ','Things I am thinking about'),
+        ],
+        [
+          new TextNode(
+            '- How people can navigate the space? Are there points of confusion in the user journey? \n - Branding vs directional signage (Directional is priority but then you build from that. What spaces are lacking visual queues and breaking the experience of being at the conference?'),
+          new TextNode(
+            '- Make sure you are communicating what you are imagining, especially to vendors! Making visual mock ups and providing reference images are the best way to communicate your ideas. It made working asynchronously in different time zones and language barriers on this project easier.'),
+            new TextNode(
+              '- What is the attendee experience day of? Will  ex) schedule of events, staff members for questions')
+        ],
+        [
+          new TextNode(
+            ' ','The final outcome'),
+        ],
+        [
+          new ImageNode(
+            'images/gitmerge-photos.jpg')
         ],
       ],
     },
     {
-      title: 'EyeSpy.',
-      tags: ['digital', 'app', 'interactive'],
-      thumbnail: 'images/EyeSpyLogin.gif',
+      title: 'Building the Future',
+      tags: ['digital', 'web design',],
+      thumbnail: 'images/btf-dibbble.gif',
       rows: [
         [
           new TextNode(
-            'EyeSpy is a project I did with my classmate, Paige Carl, for the Harn Museum of Art in Gainesville, FL. The project challenge was to design an app for the Harn Museum that focused on the Asian collection emphasizing both mobility and sensor technology. Our solution led to the creation of an app, EyeSpy, which we prototyped with a focus on the Asian collection of the Harn but could also be expanded for future collections. \n\n EyeSpy is designed to be the Harn Museum’s interactive mystery adventure game app. Users have only 60 minutes to physically explore the museum exhibit to search for clues and figure out riddles to solve the mystery. During the project I had a large focus on the story, concepting, research, User Interface design, and illustration. Paige and I worked together to create the information architecture and user experience of the game and split screen production design. \n\n Try the prototype on [invision](http://invis.io/UX2RU5KV6)'
+            'This campaign highlights developers in the open source community. Their projects are building the future by creating things that help people around them and around the world. Every one has the ability to build the future and GitHub makes it easy for people to share ideas and work together to make it happen. \n\n See it live: https://github.com/buildingthefuture \n\n I was responsible for branding, designing, and coding the website while also overseeing the production of the on-site gallery part of the campaign at our flagship conference, Universe.'),
+          new ImageNode('images/btf-websizes.jpg',2,),
+        ],
+        [
+          new TextNode(
+            'GitHub celebrated their 10 year anniversary in 2018 so we launched a campaign that celebrated all the milestones the community has accomplished in the past 10 years. The next chapter in that story is what comes next — a pivot and look towards the next 10 years. In this phase we want to empower the community to think about what they will build and how their work can impact the world around them.','Background'),
+          new TextNode(
+            'Create a website that contains all the campaign videos and information in one place.','Task'),
+          new TextNode(
+            '- Designing parallel to the creation of the campaign videos contractors were working on. The photos and videos were a big part of the visuals but there was not room on the timeline to wait to get assets in before creating the website and gallery designs. \n - Working both on all parts of the project including design, front-end development, and environmental pieces.','Challenges')
+        ],
+        [
+          new TextNode(
+            ' ','Research and Explorations'),
+        ],
+        [
+          new ImageNode('images/btf-research.jpg'),
+          new ImageNode('images/btf-sketch.jpg'),
+        ],
+        [
+          new TextNode(
+            ' ','Wireframes'),
+        ],
+        [
+          new ImageNode('images/btf-wireframes.jpg'),
+        ],
+        [
+          new TextNode(
+            ' ','Mocks Ups'),
+          new ImageNode('images/btf-typeexplorations.png',2,),
+        ],
+        [
+          new ImageNode('images/btf-whitemockup.jpg'),
+        ],
+        [
+          new TextNode(
+            ' ','Brand Definition'),
+        ],
+        [
+          new ImageNode('images/btf-branddefinition.png'),
+        ],
+        [
+          new TextNode(
+            ' ','Final Mockup'),
+        ],
+        [
+          new ImageNode('images/btf-mockupdark.jpg'),
+        ],
+        [
+          new TextNode(
+            ' ','Build'),
+        ],
+        [
+          new ImageNode('images/btf-site1.png'),
+          new ImageNode('images/btf-featureflag.png'),
+        ],
+        [
+          new ImageNode('images/btf-jamica.png'),
+          new ImageNode('images/btf-julius.png'),
+        ],
+        [
+          new TextNode(
+            'visit https://github.com/buildingthefuture','Final Site'),
+        ],
+        [
+          new ImageNode('images/btf-fullsite.png'),
+        ],
+      ],
+    },
+    {
+      title: 'Primal Screen',
+      tags: ['interactive', 'digital', 'illustration'],
+      thumbnail: 'images/nickjr-dressup.png',
+      rows: [
+        [
+          new TextNode(
+            'Primal Screen is an award winning design studio based in Atlanta, Georgia. That provide a full gamut of design service including animation, motion graphics, live action, audio, music composition, and interactive development.\n\n While I was working at Primal I was able to work on a variety of projects for clients like:\n - PBS kids \n - Sesame Workshop \n- Adult Swim \n- Nick Jr','TLDR'),
+          new ImageNode(
+            'images/Never_Give_Up.jpg',
+            2,
           ),
-          new ImageNode('images/EyeSpyRiddler.jpg'),
         ],
         [
-          new ImageNode('images/eyespy-user.jpg'),
-          new ImageNode('images/EyeSpy-elephant.jpg'),
-          new ImageNode('images/EyeSpy-menu.jpg'),
-          new ImageNode('images/EyeSpy-Concept.jpg'),
+          new TextNode(
+            'Project: Daniel Tiger\'s Neighborhood app \n\n My contributions: User Interface, User Experience, Information Architecture, Prototyping \n\n Background: Daniel Tiger is an animated family-oriented television series based on the Neighborhood of Make Believe from Mister Roger\'s Neighborhood. The show is aimed to teach children emotional intelligence and human respect. The show has produced numerous clips of their lessons and wanted a way for parents to access those videos in an app. I worked on designing the screens and interface of the app then later animated and created a prototype. One of biggest challenges we faced was accessibility. The audience was aimed for all parents of varying financial situations we had to keep in mind how videos and content could be loaded and still mobile without eating too much data. Unfortunately, I left Primal Screen before finishing this project.  ','Client: PBS Kids'),
+          new TextNode(
+            'Project: Breath, Think, Do E-book \n\n My contributions: Illustrator \n\n Background: Breathe, Think, Do is a campaign by Sesame Street that teaches children to deal with emotions like anger and frustration. Primal Screen previously created the Breath, Think, Do app for Sesame Workshop and asked this app be translated into a story and illustrated for an E-book. I was responsible for transposing all the imagery from the app to a more textured and painterly style for the E-book. ','Client: Sesame Workshop'),
+
         ],
         [
-          new ImageNode('images/EyeSpy-FlatPurple.jpg'),
-          new ImageNode('images/eyespy-infoarchitecture.jpg'),
+          new TextNode(
+            'Project: Squidbillies Bumper \n My contributions: Character design \n Background: \n\n Primal Screen has a long standing relationship with Cartoon Network also based in Atlanta. For this project we worked to create a season premier bumper for Adult Swim\'s show Squidbillies, an adult animated comedy about anthropomorphic hillbilly mud squids. The idea was to create a Godzilla inspired bumper with the main characters of the show destroying the city and causing chaos. I helped turn them from giant squids to giant gross monsters.','Client: Adult Swim'),
+            new VideoNode(
+              'https://www.youtube.com/embed/W5r4UJZBM9c',
+              'squidbillies',
+              2,
+            ),
         ],
         [
-          new ImageNode('images/EyeSpy-Vessel.jpg'),
-          new ImageNode('images/EyeSpy_Welcome.jpg'),
-          new ImageNode('images/EyeSpy-done.jpg'),
-          new ImageNode('images/EyeSpy-DemoView.jpg'),
+          new ImageNode('images/squidbillies-design.jpg'),
+        ],
+        [
+          new TextNode(
+            'Project: Bubble Guppies Kideo \n\n My contributions: Storyboard, stage hand on set, asset design, typography, title card design and motion graphics \n\n Background: \n Nickelodeon has been a long time client of Primal Screen. This bumper was one of several videos we created to help them to promote a variety of their Nick Jr. shows including Bubble Guppies, Shimmer and Shine, Team Umizoomi, Blaze and the Monster Machine, and Paw Patrol. For this video we made a mini music video using a song from the show Bubble Guppies. While planning for the video I created all the animated assets and type and helped storyboard the shots we would need on set. On set I assisted the art director and worked to help direct the kids. In post production I animated the title card of the video.','Client: Nickelodeon'),
+          new VideoNode(
+            'https://www.youtube.com/embed/95pfhGG7OXM',
+            'bubbleguppies',2,),
+        ],
+        [
+          new TextNode(
+            'Project: Nick Jr Dress up - Paw Patrols & General \n\n My contributions: Storyboard, stage hand on set, asset creation, rotoscoping, title card design and animation. \n\n Background: \n The inspiration for these videos were augmented reality face filters originally made popular by snapchat. We produced a video for their Nick Jr. shows Bubble Guppies, Shimmer and Shine, Team Umizoomi, Blaze and the Monster Machine, and Paw Patrol and another specifically for the just for the show Paw Patrol. During pre-production I rotoscoped all the assets they would use to overlay as filters and helped storyboard the shots we would need on set. On set I assisted the art director and worked to help direct the kids. During post production I designed and animated the title cards for both videos.','Client: Nickelodeon'),
+          new VideoNode(
+            'https://www.youtube.com/embed/prYG8uOQxuU',
+            'dressup',2,),
+        ],
+        [
+          new VideoNode(
+            'https://www.youtube.com/embed/AEjA3ooi180',
+            'pawpatrol'),
+        ],
+      ],
+    },
+    {
+      title: 'Jelly Belly',
+      tags: ['branding', 'illustration', 'print'],
+      thumbnail: 'images/jelly-bag.jpg',
+      rows: [
+        [
+          new TextNode(
+            'This project is the beginning of a rebrand for Jelly Belly, the makers of gourmet jelly beans. After researching Jelly Belly\'s history I learned that jelly beans were actually made popular by President Ronald Reagan during his time in office. Since then Jelly Belly has kept their brand identity the same so I wanted to bring new life to the brand and make it more relevant to today. As the generation of Ronald Reagan ages I wanted to focus to a new and younger audience. Here I have have created a new logo, mascot, flavor guide, and packaging concept. The project will later extend to show character animations and a larger packaging system.','TLDR'),
+          new ImageNode(
+            'images/jelly-all.jpg',
+            2,
+          ),
+        ],
+        [
+          new ImageNode('images/jelly-flavorbag.jpg'),
+          new ImageNode('images/jelly-cards.jpg'),
+        ],
+        [
+          new ImageNode('images/jelly-bag.jpg'),
+          new ImageNode('images/jelly-flavordetails.jpg',2,),
+        ],
+        [
+          new ImageNode('images/jelly-bagdance.gif'),
+          new ImageNode('images/jelly-bags.jpg'),
         ],
       ],
     },
@@ -83,7 +256,7 @@ class Data {
           new ImageNode('images/findfit-multiple.jpg'),
           new ImageNode('images/findfit-double.jpg'),
           new ImageNode('images/findfit-social.jpg'),
-          
+
         ],
         [
           new ImageNode('images/findfit-booklet.gif'),
@@ -111,18 +284,19 @@ class Data {
         ],
         [
           new ImageNode('images/relientk-posters.jpg'),
-          new ImageNode('images/relientk-trio.jpg'),
-          new ImageNode('images/relientk-cd.jpg'),
-        ],
-        [
-          new ImageNode('images/relientk-book.jpg'),
-          new ImageNode('images/relientk-bookdetails.jpg'),
-          new ImageNode('images/relientk-pass.jpg'),
         ],
         [
           new ImageNode('images/relientk-lyricbook.jpg'),
+          new ImageNode('images/relientk-trio.jpg'),
+        ],
+        [
+          new ImageNode('images/relientk-cd.jpg'),
+          new ImageNode('images/relientk-pass.jpg'),
+          new ImageNode('images/relientk-book.jpg'),
+        ],
+        [
           new ImageNode('images/relientk-all2.jpg'),
-        ]
+        ],
       ],
     },
     {
@@ -218,21 +392,6 @@ class Data {
       ],
       description:
         'The marketing team for Recreational Sports at the University of Florida is comprised of designers, videographers, photographers, and media specialists. I work in-house where we service many different departments within RecSports like clients making sure to boost visibility and spread awareness of several campaigns or events. This project is a campaign for 2016 - 2017 Intramural Sports. For this campaign I took advantage of all the resources and talent available. As the creative director and lead designer I worked with our photographers Camron and Muxi to take photos of over 20 different sports offered. From there I developed a style for the campaign and designed the materials for Summer 2016. I will be training and passing on this project to younger designers so the style can be applied to the campaign for Fall and Spring Semesters.'
-    },
-    {
-      title: 'Jelly Belly',
-      tags: ['branding', 'illustration', 'print'],
-      thumbnail: 'images/jelly-bag.jpg',
-      images: [
-        'images/jelly-all.jpg',
-        'images/jelly-flavordetails.jpg',
-        'images/jelly-cards.jpg',
-        'images/jelly-poster.gif',
-        'images/jelly-flavorbag.jpg',
-        'images/jelly-bagdance.gif'
-      ],
-      description:
-        "This student project is the beginning of a rebrand for Jelly Belly, the makers of gourmet jelly beans. After researching Jelly Belly's history I learned that jelly beans were actually made popular by President Ronald Reagan during his time in office. Since then Jelly Belly has kept their brand identity the same so I wanted to bring new life to the brand and make it more relevant to today. As the generation of Ronald Reagan ages I wanted to focus to a new and younger audience. Here I have have created a new logo, mascot, flavor guide, and packaging concept. The project will later extend to show character animations and a larger packaging system."
     },
     {
       title: 'Goode Chocolate Bars',
