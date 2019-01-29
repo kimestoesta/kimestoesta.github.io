5ad41636b145918ce57cc6acf506ab64451c19c2
kimestoesta <kimestoesta@github.com>
2019-01-28 22:15:04 -0800

    added hero shit

--- kim_portfolio/src/components/main/portfolio/Portfolio.js
+++ kim_portfolio/src/components/main/portfolio/Portfolio.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Footer from '../footer/Footer';
 import PortfolioStuff from './PortfolioStuff';
+import PortfolioHero from './PortfolioHero';
 
 class Portfolio extends Component {
   render() {
     return (
       <div>
+        <PortfolioHero />
         <PortfolioStuff />
         <Footer />
       </div>
--- kim_portfolio/src/components/main/portfolio/PortfolioHero.js
+++ kim_portfolio/src/components/main/portfolio/PortfolioHero.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import styles from './PortfolioHero.scss';
 
 class PortfolioHero extends Component {
   render() {
     return (
-      <div align="center" className="text-dark">
+      <div className="text-dark mx-auto">
         <h4 className="m-b-50"> Hello! My Name is</h4>
         <h1 className="m-b-30"> Kim Estoesta</h1>
-        <h5 className="m-b-60"> Love me. pls.</h5>
+        <h5 className="m-6"> Love me. pls.</h5>
         <a href="#portfolio" className="btn btn-dark anim-scroll">
           View Work
         </a>
