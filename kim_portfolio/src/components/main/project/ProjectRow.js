5bed477d4c8e0ebd79638d47998603dfca98d75a
Nick Jiang <njiang1209@ufl.edu>
2019-01-29 22:09:00 -0800

    resp

--- kim_portfolio/src/components/main/project/ProjectRow.js
+++ kim_portfolio/src/components/main/project/ProjectRow.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import styles from './ProjectRow.module.scss';
 
 class ProjectRow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth});
+  }
+
   render() {
     var totalCols = this.props.row.reduce((accumulator, node) => {
       return accumulator + (node.columns ? node.columns : 1);
@@ -10,8 +29,9 @@ class ProjectRow extends Component {
     var nodes = this.props.row.map((node, index) => {
       var cols = node.columns ? node.columns : 1;
 
+      console.log(this.state.width);
       const widthStyles = {
-        width: (100 * cols) / totalCols + '%'
+        width: (this.state.width < 768 ? 100 : (100 * cols) / totalCols) + '%'
       };
 
       return (
