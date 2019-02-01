441411838f3583dad5fae0e9fc57d29f6036af5a
Nick Jiang <njiang1209@ufl.edu>
2019-02-01 00:01:35 -0800

    scrolltotop

--- kim_portfolio/src/App.js
+++ kim_portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Sidebar from './components/sidebar/Sidebar';
+import ScrollToTop from './ScrollToTop';
 import Main from './components/main/Main';
 import { HashRouter } from 'react-router-dom';
 import './App.scss';
@@ -8,10 +9,12 @@ class App extends Component {
   render() {
     return (
       <HashRouter>
-        <div className="App">
-          <Sidebar />
-          <Main />
-        </div>
+        <ScrollToTop>
+          <div className="App">
+            <Sidebar />
+            <Main />
+          </div>
+        </ScrollToTop>
       </HashRouter>
     );
   }
