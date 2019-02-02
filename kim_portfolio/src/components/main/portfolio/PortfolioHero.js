5f3dbc8616836c207a44ba23cd457439c77e424e
kimestoesta <kimestoesta@github.com>
2019-02-02 11:07:05 -0800

    love me. pls

--- kim_portfolio/src/components/main/portfolio/PortfolioHero.js
+++ kim_portfolio/src/components/main/portfolio/PortfolioHero.js
@@ -6,7 +6,7 @@ class PortfolioHero extends Component {
     return (
       <div className="heroheight col-md-12 col-lg-10 mx-auto text-center">
         <h1 className="mb-2">Hi I'm Kim</h1>
-        <p className="mb-5">Love me. Pls.</p>
+        <p className="mb-5">love me. pls.</p>
       </div>
     );
   }
